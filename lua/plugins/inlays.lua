89ab19755a8e2988a19ddce2ed24affde5dab4b2
Dave Brudner <dave@gridline.co>
2024-07-01 09:41:23 -0500

    Fix disable inlays

--- lua/plugins/inlays.lua
+++ lua/plugins/inlays.lua
@@ -3,11 +3,4 @@ return {
   opts = {
     inlay_hints = { enabled = false },
   },
-
-  {
-    "nvim-treesitter/nvim-treesitter",
-    dependencies = {
-      { "windwp/nvim-ts-autotag", opts = {} },
-    },
-  },
 }
