47869f8ecff73363a90f119511386c8e98fcd2c2
Dave Brudner <dave@gridline.co>
2024-07-11 09:13:37 -0500

    git_scommit

--- lua/custom/git_scommit.lua
+++ lua/custom/git_scommit.lua
@@ -6,19 +6,40 @@ local conf = require("telescope.config").values
 
 local previewers = require("telescope.previewers")
 local Previewer = previewers.new_buffer_previewer
+--
+-- Function to get the current branch name
+local function get_current_branch()
+  local handle = io.popen("git rev-parse --abbrev-ref HEAD")
 
--- Creating a custom previewer for the git log
-local git_log_previewer = function(opts)
+  if handle == nil then
+    return ""
+  end
+
+  local branch = handle:read("*a"):gsub("\n", "")
+  handle:close()
+  return branch
+end
+
+-- Creating a custom previewer for the git diff
+local git_diff_previewer = function()
   return Previewer({
-    title = "Git Log",
-    define_preview = function(self, entry, status)
+    title = "Git Diff",
+    define_preview = function(self, entry)
       if not entry.value then
         return
       end
+      -- Extract commit hash from entry.value
+      local commit_hash = entry.value:match("^(%S+)")
+      if not commit_hash then
+        return
+      end
+
       -- Clearing the buffer
       vim.api.nvim_buf_set_lines(self.state.bufnr, 0, -1, false, {})
-      -- Fetching git log for the branch with a shorter commit hash
-      local command = "git log --pretty=format:'- %s %h' -n 10 " .. entry.value
+
+      -- Fetching git diff for the commit
+      local current_branch = get_current_branch()
+      local command = "git diff " .. current_branch .. " " .. commit_hash
       local handle = io.popen(command, "r")
       if handle then
         local results = {}
@@ -33,44 +54,33 @@ local git_log_previewer = function(opts)
   })
 end
 
--- Function to get the list of branches from git reflog
-local function get_git_branches()
-  local command =
-    [[git reflog --date=iso --pretty='%gd: %gs' | grep 'checkout: moving from ' | sed -E 's/.*checkout: moving from (.*) to (.*)/\2/' | grep -vE 'HEAD~?[0-9]*|^refs/heads/|^[0-9a-f]{7,40}$' | nl | sort -uk2,2 | sort -nr | cut -f2-]]
+-- Function to get the list of commits from git log
+local function get_git_commits()
+  local command = "git log --pretty=format:'%h %s'"
   local handle = io.popen(command)
-
-  if handle == nil then
-    return {}
-  end
-
   local result = handle:read("*a")
   handle:close()
 
-  local branches = {}
-  for branch in result:gmatch("[^\r\n]+") do
-    table.insert(branches, branch)
+  local commits = {}
+  for commit in result:gmatch("[^\r\n]+") do
+    table.insert(commits, commit)
   end
 
-  local reversed_branches = {}
-  for i = #branches, 1, -1 do
-    table.insert(reversed_branches, branches[i])
-  end
-
-  return reversed_branches
+  return commits
 end
 
--- Telescope picker for git branches
-function _G.git_reslog()
-  local branches = get_git_branches()
+-- Telescope picker for git commits
+function Git_commits()
+  local commits = get_git_commits()
 
   pickers
     .new({}, {
-      prompt_title = "Git ReSlog",
+      prompt_title = "Git Commits",
       finder = finders.new_table({
-        results = branches,
+        results = commits,
       }),
       sorter = conf.generic_sorter({}),
-      previewer = git_log_previewer(),
+      previewer = git_diff_previewer(),
       layout_strategy = "flex",
       layout_config = {
         horizontal = {
@@ -85,13 +95,11 @@ function _G.git_reslog()
       },
       attach_mappings = function(prompt_bufnr, map)
         local function on_select()
-          local selected_branch = action_state.get_selected_entry().value
+          local selected_commit = action_state.get_selected_entry().value
           actions.close(prompt_bufnr)
-          -- Commit changes with a generic message if there are any
-          os.execute("git add .")
-          os.execute("HUSKY=0 git commit -m 'Auto commit before branch switch'")
-          -- Switch to the selected branch
-          os.execute("git checkout " .. selected_branch)
+          -- Checkout the selected commit (if needed)
+          -- os.execute("git checkout " .. selected_commit)
+          -- Or perform any other action with the selected commit
         end
 
         map("i", "<CR>", on_select)
@@ -103,4 +111,4 @@ function _G.git_reslog()
 end
 
 -- Command to invoke the custom picker
-vim.api.nvim_set_keymap("n", "<leader>gr", "<cmd>lua git_reslog()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>gc", "<cmd>lua Git_commits()<CR>", { noremap = true, silent = true })
