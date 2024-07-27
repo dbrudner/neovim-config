372d461dcf0d1137093fc726df73c98a35d0c169
Dave Brudner <dave@gridline.co>
2024-07-27 18:30:04 -0500

    back to cyberdream

--- lua/plugins/colorschema.lua
+++ lua/plugins/colorschema.lua
@@ -1,21 +1,26 @@
 return {
-  "scottmckendry/cyberdream.nvim",
-  lazy = false,
-  priority = 1000,
-  transarent = true,
-  config = function()
-    require("cyberdream").setup({
-      transparent = true,
-    })
-  end,
+
   {
-    "tokyonight.nvim",
-    opts = {
-      transparent = true,
-      styles = {
-        sidebars = "transparent",
-        floats = "transparent",
-      },
-    },
+    "scottmckendry/cyberdream.nvim",
+    lazy = false,
+    priority = 1000,
+    transarent = true,
+    config = function()
+      require("cyberdream").setup({
+        transparent = true,
+      })
+
+      vim.cmd("colorscheme cyberdream")
+    end,
   },
+  -- {
+  --   "tokyonight.nvim",
+  --   opts = {
+  --     transparent = true,
+  --     styles = {
+  --       sidebars = "transparent",
+  --       floats = "transparent",
+  --     },
+  --   },
+  -- },
 }
