1dbeffc662457667b345f3fe7e2df9fd1c3dcd1e
Dave Brudner <dave@gridline.co>
2024-08-18 10:14:25 -0500

    templ format on save

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,11 +2,9 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
-return {
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      inlay_hints = { enabled = false },
-    },
-  },
-}
+vim.cmd([[
+  augroup TemplFmt
+    autocmd!
+    autocmd BufWritePost *.templ silent! execute "!PATH=\"$PATH:$(go env GOPATH)/bin\" templ fmt <afile> >/dev/null 2>&1" | redraw!
+  augroup END
+]])
